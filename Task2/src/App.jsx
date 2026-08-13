78315dd9d2dcd79ac0be3dfef22669ab07910989
Rohan <rohantatipamul01@gmail.com>
2026-08-13 17:37:48 +0530

    Task 2 Completed

---
+++ Task2/src/App.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {useState} from "react";
+function App()
+{
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTask()
+  {
+    if(task.trim() !== "")
+    {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  }
+  
+  function deleteTask()
+  {
+    setTodos([]);
+  }
+
+  function deleteTaskById(index)
+  {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return(
+    <>
+      <h1>To Do Application</h1>
+
+      <input type="text" placeholder="Enter your task" value={task} onChange={(e)=>setTask(e.target.value)}/>
+
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTaskById(index)}>Delete</button>         
+          </li>
+        ))}
+
+      </ul>
+
+      <button onClick={() => deleteTaskById(todos.length - 1)}>Delete</button>
+      <button onClick={deleteTask}>Delete ALL</button>
+    </>
+  );
+}
+
+export default App;
